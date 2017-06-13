7118a081a0c4760e08c31a0c1925f7c2acc5c262
Jerry <exnxs@hotmail.com>
2017-06-12 21:38:53 -0400

    Wire up the CountdownForm

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -24,7 +24,6 @@ var Clock = React.createClass({
   },
   render: function(){
     var {totalSeconds} = this.props;
-    console.log(this.props.totalSeconds);
 
     return (
       <div className="clock">
---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+
+var CountDownForm = React.createClass({
+  onSubmit: function(e){
+    e.preventDefault();
+    var strSeconds = this.refs.seconds.value;
+
+    if(strSeconds.match(/^[0-9]*$/)){
+      this.refs.seconds.value = '';
+      this.props.onSetCountdown(parseInt(strSeconds, 10));
+    }
+  },
+  render: function(){
+    return (
+      <form ref="form" onSubmit={this.onSubmit} class="countdown-form">
+        <input type="text" ref="seconds" placeholder="set your countdown"/>
+        <button className="button expanded" type="submit">Start</button>
+      </form>
+    )
+  }
+});
+
+module.exports = CountDownForm;
